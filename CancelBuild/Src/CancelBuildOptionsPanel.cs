827e9b39e9ad8d9eb0604ad3689eb021f0c81eec
brahn <brahn@clarionedge.com>
2011-05-13 12:46:07 +0000

    Initial commit of the CancelBuild addin. Currently at v0.4

---
+++ CancelBuild/Src/CancelBuildOptionsPanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop;
+using ICSharpCode.SharpDevelop.Gui;
+using System.Diagnostics;
+
+namespace ClarionEdge.CancelBuildButtons
+{
+    public partial class CancelBuildOptionsPanel : AbstractOptionPanel
+    {
+
+        public CancelBuildOptionsPanel()
+        {
+            InitializeComponent();
+            //baseOptionsPanel.Image = CommonResources.Properties.famfamfam_silk.famfamfam_silk_picture_empty;
+        }
+
+        public override void LoadPanelContents()
+        {
+            hideButtonLabels.Checked = PropertyService.Get("CancelBuildButtons.Options.HideButtonLabels", false);
+            
+        }
+
+        public override bool StorePanelContents()
+        {
+            PropertyService.Set("CancelBuildButtons.Options.HideButtonLabels", hideButtonLabels.Checked);
+            return true;
+        }
+
+    }
+}
