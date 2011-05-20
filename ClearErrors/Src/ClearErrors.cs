8e370d9a8955cdac4085d6449f1b9a98a40974dd
brahn <brahn@clarionedge.com>
2011-05-20 19:15:43 +0000

    v0.4 - Preparing for public release of source code. Making the VS project work without dependancies, adding docs, etc

---
+++ ClearErrors/Src/ClearErrors.cs
@@ -0,0 +1,44 @@
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop;
+using ICSharpCode.SharpDevelop.Gui;
+
+namespace ClarionEdge.ClearErrors
+{
+    public class ClearErrors : AbstractMenuCommand
+    {
+        public override void Run()
+        {
+            TaskService.Clear();
+        }
+    }
+
+    public class SelectAllErrors : AbstractMenuCommand
+    {
+
+        public override void Run()
+        {
+            PadDescriptor index = WorkbenchSingleton.Workbench.GetPad(typeof(ErrorListPad));
+            if (index != null)
+            {
+                index.BringPadToFront();
+                ((ErrorListPad)index.PadContent).SelectAll();
+            }
+
+        }
+    }
+    public class CopyAllErrors : AbstractMenuCommand
+    {
+
+        public override void Run()
+        {
+            PadDescriptor index = WorkbenchSingleton.Workbench.GetPad(typeof(ErrorListPad));
+            if (index != null)
+            {
+                index.BringPadToFront();
+                ((ErrorListPad)index.PadContent).SelectAll();
+                ((ErrorListPad)index.PadContent).Copy();
+            }
+
+        }
+    }
+}
