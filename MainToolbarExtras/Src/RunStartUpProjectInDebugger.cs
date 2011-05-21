903d3b3f3cfc94f4f3d937f19d66b14bf584c58a
brahn <brahn@clarionedge.com>
2011-05-21 09:54:47 +0000

    Release 0.4, 21 May 2011 CHANGE:	Preparing for public release of source code. Making the VS project work without dependencies, adding docs, etc
    Release 0.3, 09 May 2011
    FEATURE:	Optionally set the size for ALL accessible toolbars in the IDE!
    BLOG:	MainToolbarExtras addin v0.3 – Set ALL Toolbar Sizes!
    
    Release 0.2, 01 May 2011
    FEATURE:	You can now define the size of the main toolbar and icons!
    BLOG:	MainToolbarExtras addin v0.2–Set Main Toolbar Size!
    
    Release 0.1, 02 March 2011
    INFO:	Initial Release
    FEATURE:	Puts a new button on the main toolbar that will launch the “StartUp Project” in the debugger without building the solution first.
    BLOG:	MainToolbarExtras addin v0.1 – Run Startup Project in the debugger

---
+++ MainToolbarExtras/Src/RunStartUpProjectInDebugger.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Clarion.Core.Redirection;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.Debugging;
+using ICSharpCode.SharpDevelop.Project;
+using SoftVelocity.Common;
+
+namespace ClarionEdge.MainToolbarExtras
+{
+    public class RunStartUpProjectInDebugger : AbstractMenuCommand
+    {
+        // NOTE: This code is largely a copy of SoftVelocity.Generator.Commands.RunProject
+        // It has been stripped down a little as it is only needed to run the debugger.
+        // It would probably be a good idea to check agains the original code on new updates to make sure it will still work!
+
+        private bool _useDebug;
+
+        public override void Run()
+        {
+            UseDebug = true;
+            DoRun(ProjectService.OpenSolution.StartupProject);
+        }
+
+        protected void DoRun(IProject project)
+        {
+            RedirectionFile redFile = CommonClarionProject.CurrentRedirectionFile(project);
+            if (project is CompilableProject)
+            {
+                try
+                {
+                    CompilableProject project2 = (CompilableProject)project;
+                    string path = null;
+                    switch (project2.StartAction)
+                    {
+                        case StartAction.Project:
+                            if (Path.GetExtension(project2.OutputAssemblyFullPath).ToUpper() == ".EXE")
+                            {
+                                path = redFile.OpenName(project2.OutputAssemblyFullPath, project2.Directory);
+                                string startArguments = project2.StartArguments;
+                                Path.GetDirectoryName(path);
+                            }
+                            break;
+
+                        case StartAction.Program:
+                            {
+                                path = redFile.OpenName(project2.StartProgram, "");
+                                string text1 = project2.StartArguments;
+                                break;
+                            }
+                        case StartAction.StartURL:
+                            path = project2.StartArguments;
+                            break;
+                    }
+                    if (path != null)
+                    {
+                        project.Start(this.UseDebug);
+                    }
+                }
+                catch (FileNotFoundException exception)
+                {
+                    MessageBox.Show(string.Format(ResourceService.GetString("Clarion.Generator.FileNotFoundExceptionMessage"), exception.FileName), ResourceService.GetString("Clarion.Generator.FileNotFoundExceptionCaption"));
+                }
+            }
+            else
+            {
+                this.DoRun(project.OutputAssemblyFullPath, null, null);
+            }
+        }
+
+        protected void DoRun(string exeName, string arguments, string workingDir)
+        {
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = exeName
+                };
+                if ((workingDir != null) && Directory.Exists(workingDir))
+                {
+                    processStartInfo.WorkingDirectory = workingDir;
+                }
+                if (!string.IsNullOrEmpty(arguments))
+                {
+                    processStartInfo.Arguments = arguments;
+                }
+                if (this.UseDebug)
+                {
+                    DebuggerService.CurrentDebugger.Start(processStartInfo);
+                }
+                else
+                {
+                    DebuggerService.CurrentDebugger.StartWithoutDebugging(processStartInfo);
+                }
+            }
+            catch (Win32Exception exception)
+            {
+                if (exception.ErrorCode == -2147467259)
+                {
+                    MessageBox.Show(exception.Message, ResourceService.GetString("Clarion.Generator.FileNotFoundExceptionCaption"));
+                }
+            }
+            catch (FileNotFoundException exception2)
+            {
+                MessageBox.Show(string.Format(ResourceService.GetString("Clarion.Generator.FileNotFoundExceptionMessage"), exception2.FileName), ResourceService.GetString("Clarion.Generator.FileNotFoundExceptionCaption"));
+            }
+            catch (Exception exception3)
+            {
+                MessageBox.Show(exception3.Message, ResourceService.GetString("Clarion.Generator.FileNotFoundExceptionCaption"));
+            }
+        }
+
+        public bool UseDebug
+        {
+            get
+            {
+                return this._useDebug;
+            }
+            set
+            {
+                this._useDebug = value;
+            }
+        }
+    }
+}
---
+++ MainToolbarExtras/Src/SetToolbarSize.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Forms;
+using ICSharpCode.Core;
+
+namespace ClarionEdge.MainToolbarExtras
+{
+    public partial class SetToolbarSize : Form
+    {
+        public SetToolbarSize()
+        {
+            InitializeComponent();
+            tbIconSize.Text = PropertyService.Get("ClarionEdge.MainToolbarExtras.iconSize", 24).ToString();
+            tbToolbarHeight.Text = PropertyService.Get("ClarionEdge.MainToolbarExtras.toolbarHeight", 42).ToString();
+        }
+
+        private void buttonOK_Click(object sender, System.EventArgs e)
+        {
+            PropertyService.Set("ClarionEdge.MainToolbarExtras.iconSize", tbIconSize.Text);
+            PropertyService.Set("ClarionEdge.MainToolbarExtras.toolbarHeight", tbToolbarHeight.Text);
+        }
+
+        private void buttonHelp_Click(object sender, System.EventArgs e)
+        {
+            MessageService.ShowMessage("Original dimensions are: \n\n" +
+                "(Clarion 7)\n" + 
+                "Icon Size = 16\n" +
+                "ToolBar Height = 25\n\n" +
+                "(Clarion 8)\n" + 
+                "Icon Size = 24\n" + 
+                "ToolBar Height = 42");
+        }
+    }
+}
