5e8f78e7b40eaa04c77460f72134f439aebf0b95
brahn <brahn@clarionedge.com>
2011-05-21 09:41:36 +0000

    Release 0.4, 21 May 2011 CHANGE:	Preparing for public release of source code. Making the VS project work without dependancies, adding docs, etc
    Release 0.3, 16 Oct 2010
    FEATURE:	Show Additional Indentation – When set to true, displays Property indentations as the Microsoft PropertyGrid does (except for the first level whose +/- glyphs won’t appear in the left most column).
    FEATURE:	Auto Adjust Label Column – Resizes the label column to adjust itself to the longest label.
    FEATURE:	Themes (using the DrawManger) – There are 3 build in themes and an additional Dark theme that I have borrowed from the SmartPropertyGrid code example.
    BLOG:	PropertyGridExtras addin v0.3 – now with Additional Settings!
    
    Release 0.2, 03 March 2011
    FIX:	There was an issue with restoring the saved font using the incorrect Font.Unit in some circumstances.”
    
    Release 0.1, 03 March 2011
    INFO:	Initial Release
    BLOG:	PropertyGridExtras addin v0.1 – Grow/Shrink font size

---
+++ PropertyGridExtras/Src/FontSizeCommands.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.Gui;
+using VisualHint.SmartPropertyGrid;
+
+namespace ClarionEdge.PropertyGridExtras
+{
+    class GrowFont : AbstractMenuCommand
+    {
+        public override void Run()
+        {
+            PropertyGridSV grid = PropertyPad.Grid;
+            grid.Font = new Font(grid.Font.Name, grid.Font.Size + 1.0f, grid.Font.Style, grid.Font.Unit);
+            PropertyGridHelper.Refresh(grid, grid.Font.Size.ToString());
+        }
+    }
+
+    class ShrinkFont : AbstractMenuCommand
+    {
+        public override void Run()
+        {
+            PropertyGridSV grid = PropertyPad.Grid;
+            grid.Font = new Font(grid.Font.Name, grid.Font.Size - 1.0f, grid.Font.Style, grid.Font.Unit);
+            PropertyGridHelper.Refresh(grid, grid.Font.Size.ToString());
+        }
+    }
+    class ResetFont : AbstractMenuCommand
+    {
+        public override void Run()
+        {
+            Form mf = WorkbenchSingleton.MainForm;
+            PropertyGridSV grid = PropertyPad.Grid;
+            grid.Font = new Font(grid.Font.Name, 
+                mf.Font.Size,
+                grid.Font.Style, 
+                mf.Font.Unit);
+            PropertyGridHelper.Refresh(grid, grid.Font.Size.ToString(), true);
+        }
+    }
+
+}
