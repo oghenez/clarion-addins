7fc297f24b2388be53bbde36e1e963ec225d8690
brahn@clarionedge.com <brahn@clarionedge.com@b60aab8c-cd5c-1699-4943-ff1c4fd65dd7>
2011-05-21 09:41:36 +0000

    Release 0.4, 21 May 2011 CHANGE:	Preparing for public release of source code. Making the VS project work without dependancies, adding docs, etc
    Release 0.3, 16 Oct 2010
    FEATURE:	Show Additional Indentation – When set to true, displays Property indentations as the Microsoft PropertyGrid does (except for the first level whose +/- glyphs won’t appear in the left most column).
    FEATURE:	Auto Adjust Label Column – Resizes the label column to adjust itself to the longest label.
    FEATURE:	Themes (using the DrawManger) – There are 3 build in themes and an additional Dark theme that I have borrowed from the SmartPropertyGrid code example.
    BLOG:	PropertyGridExtras addin v0.3 – now with Additional Settings!
    
    Release 0.2, 03 March 2011
    FIX:	There was an issue with restoring the saved font using the incorrect Font.Unit in some circumstances.”
    
    Release 0.1, 03 March 2011
    INFO:	Initial Release
    BLOG:	PropertyGridExtras addin v0.1 – Grow/Shrink font size

---
+++ trunk/PropertyGridExtras/Src/AdditionalSettingsCommands.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using ICSharpCode.Core;
+using VisualHint.SmartPropertyGrid;
+
+namespace ClarionEdge.PropertyGridExtras
+{
+
+    class AutoAdjustLabelColumn : AbstractCheckableMenuCommand
+    {
+        public override bool IsChecked
+        {
+            get
+            {
+                return Convert.ToBoolean(PropertyService.Get("ClarionEdge.PropertyGridExtras.AutoAdjustLabelColumn", "false"));
+            }
+            set
+            {
+                PropertyService.Set("ClarionEdge.PropertyGridExtras.AutoAdjustLabelColumn", Convert.ToString(value));
+                PropertyGridHelper.AutoAdjustLabelColumn();
+            }
+        }
+    }
+
+    class ShowAdditionalIndentation : AbstractCheckableMenuCommand
+    {
+        public override bool IsChecked
+        {
+            get
+            {
+                return Convert.ToBoolean(PropertyService.Get("ClarionEdge.PropertyGridExtras.ShowAdditionalIndentation", "false"));
+            }
+            set
+            {
+                PropertyService.Set("ClarionEdge.PropertyGridExtras.ShowAdditionalIndentation", Convert.ToString(value));
+                PropertyGridHelper.ShowAdditionalIndentation();
+            }
+        }
+    }
+
+    class UseDefaultDrawManager : AbstractCheckableMenuCommand
+    {
+        public override bool IsChecked
+        {
+            get
+            {
+                return Convert.ToBoolean(PropertyService.Get("ClarionEdge.PropertyGridExtras.UseDefaultDrawManager", "false"));
+            }
+            set
+            {
+                PropertyService.Set("ClarionEdge.PropertyGridExtras.UseDefaultDrawManager", Convert.ToString(value));
+                PropertyGridHelper.SetDrawManager(PropertyGrid.DrawManagers.DefaultDrawManager, value);
+            }
+        }
+    }
+
+    class UseLightDrawManager : AbstractCheckableMenuCommand
+    {
+        public override bool IsChecked
+        {
+            get
+            {
+                return Convert.ToBoolean(PropertyService.Get("ClarionEdge.PropertyGridExtras.UseLightDrawManager", "false"));
+            }
+            set
+            {
+                PropertyService.Set("ClarionEdge.PropertyGridExtras.UseLightDrawManager", Convert.ToString(value));
+                PropertyGridHelper.SetDrawManager(PropertyGrid.DrawManagers.LightColorDrawManager, value);
+            }
+        }
+    }
+
+    class UseCustomDrawManager : AbstractCheckableMenuCommand
+    {
+        public override bool IsChecked
+        {
+            get
+            {
+                return Convert.ToBoolean(PropertyService.Get("ClarionEdge.PropertyGridExtras.UseCustomDrawManager", "false"));
+            }
+            set
+            {
+                PropertyService.Set("ClarionEdge.PropertyGridExtras.UseCustomDrawManager", Convert.ToString(value));
+                PropertyGridHelper.SetDrawManager(PropertyGrid.DrawManagers.CustomDrawManager, value);
+            }
+        }
+    }
+
+}
+
+
---
+++ trunk/PropertyGridExtras/Src/StartupCode.cs
@@ -0,0 +1,83 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.Gui;
+using VisualHint.SmartPropertyGrid;
+
+namespace ClarionEdge.PropertyGridExtras
+{
+    class StartupCode : AbstractCommand
+    {
+
+        bool _startupDone = false;
+        public override void Run()
+        {
+            WorkbenchSingleton.WorkbenchCreated += new System.EventHandler(WorkbenchSingleton_WorkbenchCreated);
+        }
+
+        void WorkbenchSingleton_WorkbenchCreated(object sender, System.EventArgs e)
+        {
+
+            WorkbenchSingleton.Workbench.ActiveWorkbenchWindowChanged += new System.EventHandler(Workbench_ActiveWorkbenchWindowChanged);
+        }
+
+        void Workbench_ActiveWorkbenchWindowChanged(object sender, System.EventArgs e)
+        {
+            if (_startupDone == true || PropertyPad.Grid == null)
+                return;
+
+            // This event is handy for doing things when focus or pad instance changes
+            PropertyPad.SelectedObjectChanged += new System.EventHandler(PropertyPad_SelectedObjectChanged);
+            PropertyPad.SelectedGridItemChanged += new VisualHint.SmartPropertyGrid.PropertyGrid.PropertySelectedEventHandler(PropertyPad_SelectedGridItemChanged);
+
+            // Init all the additional properties
+            PropertyGridHelper.AutoAdjustLabelColumn();
+            PropertyGridHelper.ShowAdditionalIndentation();
+            PropertyGridHelper.SetDrawManager();
+
+            PropertyGridSV grid = PropertyPad.Grid;
+            grid.PropertyExpanded += new VisualHint.SmartPropertyGrid.PropertyGrid.PropertyExpandedEventHandler(grid_PropertyExpanded);
+            Form mf = WorkbenchSingleton.MainForm;
+            if (PropertyService.Get("ClarionEdge.PropertyGridExtras.FontSize", "") != "")
+            {
+                float newSize = float.Parse(PropertyService.Get("ClarionEdge.PropertyGridExtras.FontSize", ""));
+                if (PropertyService.Get("ClarionEdge.PropertyGridExtras.ResetDone", "false") == "true")
+                {
+                    grid.Font = new Font(grid.Font.Name, newSize, grid.Font.Style, mf.Font.Unit);
+                }
+                else
+                {
+                    grid.Font = new Font(grid.Font.Name, newSize, grid.Font.Style, grid.Font.Unit);
+                }
+                grid.RefreshProperties();
+            }
+            _startupDone = true;
+        }
+
+        void PropertyPad_SelectedGridItemChanged(object sender, PropertySelectedEventArgs e)
+        {
+            LoggingService.Debug("PropertyGridExtras PropertyPad_SelectedGridItemChanged");
+            if (PropertyPad.Grid.Site != null)
+                LoggingService.Debug("PropertyPad.Grid.Site=" + PropertyPad.Grid.Site.Name);
+        }
+
+        void grid_PropertyExpanded(object sender, PropertyExpandedEventArgs e)
+        {
+            LoggingService.Debug("property (" + e.PropertyEnum.Property.Id + ") " + e.PropertyEnum.Property.DisplayName + " state=" + e.Expanded.ToString());
+        }
+
+        void PropertyPad_SelectedObjectChanged(object sender, System.EventArgs e)
+        {
+            SelectedObjectChangedEventArgs ea = (SelectedObjectChangedEventArgs)e;
+            if (PropertyPad.Grid.SelectedObject != null)
+            {
+                LoggingService.Debug("PropertyPad.Grid.SelectedObject: " + PropertyPad.Grid.SelectedObject.ToString());
+            }
+            
+            if (PropertyPad.Grid.Site != null)
+                LoggingService.Debug("PropertyPad.Grid.Site=" + PropertyPad.Grid.Site.Name);
+
+            PropertyGridHelper.AutoAdjustLabelColumn();
+        }
+    }
+}
