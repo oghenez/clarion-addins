08e041e957bc1682cdbf7be71f33975fb4445ade
brahn <brahn@clarionedge.com>
2011-05-21 10:07:25 +0000

    Release 0.3, 20 May 2011 CHANGE:	Preparing for public release of source code. Making the VS project work without dependencies, adding docs, etc
    Release 0.2, 09 Oct 2010
    FEATURE:	Adds a new button to the Solution (ProjectBrowser) toolbar – Close Solution.
    BLOG:	ProjectBrowserExtras addin v0.2–Close Solution button
    
    Release 0.1, 21 Dec 2010
    INFO:	Initial Release
    FEATURE:	Adds two new buttons, Expand and Contract, to the Project Browser, otherwise known as the Solution Explorer.
    BLOG:	ProjectBrowserExtras Addin

---
+++ ProjectBrowserExtras/Src/ProjectBrowserExtrasCommands.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Forms;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.Project;
+
+namespace ClarionEdge.ProjectBrowserExtras
+{
+    public class CollapseTree : AbstractMenuCommand
+    {
+        public override void Run()
+        {
+            TreeNode node = ProjectBrowserPad.Instance.ProjectBrowserControl.RootNode;
+            if (node != null)
+            {
+                foreach (TreeNode n in node.Nodes)
+                {
+                    node.Collapse(false);
+                }
+                node.Nodes[0].Expand();
+            }
+        }
+    }
+
+    public class ExpandTree : AbstractMenuCommand
+    {
+        public override void Run()
+        {
+            TreeNode node = ProjectBrowserPad.Instance.ProjectBrowserControl.RootNode;
+            if (node != null)
+            {
+                foreach (TreeNode n in node.Nodes)
+                {
+                    node.Expand();
+                }
+                node.Nodes[0].Expand();
+            }
+        }
+    }
+}
