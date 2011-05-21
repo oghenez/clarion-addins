72087b5de5dd4f337b5e4859f8f3ae8da1ad4a6e
brahn@clarionedge.com <brahn@clarionedge.com@b60aab8c-cd5c-1699-4943-ff1c4fd65dd7>
2011-05-21 10:00:32 +0000

    Release 0.2, 21 May 2011 CHANGE:	Preparing for public release of source code. Making the VS project work without dependencies, adding docs, etc
    Release 0.1, 01 Feb 2011
    INFO:	Initial Release
    BLOG:	OptionsAutoSave addin v0.1 now available

---
+++ trunk/OptionsAutoSave/Src/OptionsCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.Gui;
+
+namespace ClarionEdge.OptionsAutoSave
+{
+    class OptionsCommand : AbstractMenuCommand
+    {
+        public override void Run()
+        {
+            using (TreeViewOptions optionsDialog = new TreeViewOptions((Properties)PropertyService.Get("ICSharpCode.TextEditor.Document.Document.DefaultDocumentAggregatorProperties", new Properties()),
+            AddInTree.GetTreeNode("/SharpDevelop/Dialogs/OptionsDialog")))
+            {
+                optionsDialog.FormBorderStyle = FormBorderStyle.FixedDialog;
+
+                optionsDialog.Owner = (Form)WorkbenchSingleton.Workbench;
+                optionsDialog.ShowDialog(ICSharpCode.SharpDevelop.Gui.WorkbenchSingleton.MainForm);
+                if (optionsDialog.DialogResult == DialogResult.OK)
+                {
+                    PropertyService.Save();
+                }
+            }
+        }
+    }
+}
