7081b6d0810ff783545c083c1ff9da114f4ca71c
brahn <brahn@clarionedge.com>
2011-06-04 15:12:59 +0000

    Release 0.3, 20 May 2011 CHANGE:	No longer messes up the editor if you try and delete the last line of an embed when in source view FEATURE:	Now supports undo FEATURE:	Now supports all text editor controls (clw, inc, tpw, tpl, etc) not just the clarion Embeditor/Embed editor CHANGE:	Menu item moved into the Edit menu. Seems to make more sense here CHANGE:	Menu item is only active when a text editor is open

--- RemoveLine/Src/RemoveLineCommand.cs
+++ RemoveLine/Src/RemoveLineCommand.cs
@@ -2,6 +2,7 @@ using ICSharpCode.Core;
 using ICSharpCode.SharpDevelop.DefaultEditor.Gui.Editor;
 using ICSharpCode.SharpDevelop.Gui;
 using ICSharpCode.TextEditor;
+using ICSharpCode.TextEditor.Document;
 using SoftVelocity.Generator.Editor;
 
 namespace ClarionEdge.RemoveLine
@@ -17,13 +18,58 @@ namespace ClarionEdge.RemoveLine
                 return;
             }
 
-            TextEditorControl textarea = ((ITextEditorControlProvider)window.ViewContent).TextEditorControl;
-            PweeLineManager customLineManager = textarea.Document.CustomLineManager as PweeLineManager;
-            int lineNumber = textarea.Document.GetLineNumberForOffset(textarea.ActiveTextAreaControl.Caret.Offset);
+            TextEditorControl textArea = ((ITextEditorControlProvider)window.ViewContent).TextEditorControl;
+            PweeLineManager customLineManager = textArea.Document.CustomLineManager as PweeLineManager;
+            if (customLineManager != null)
+            {
+                RemovePweeLine(textArea, customLineManager);
+            }
+            else
+            {
+                new ICSharpCode.TextEditor.Actions.DeleteLine().Execute(textArea.ActiveTextAreaControl.TextArea);
+            }
+        }
+
+        private void RemovePweeLine(TextEditorControl textArea, PweeLineManager customLineManager)
+        {
+            int lineNumber = textArea.Document.GetLineNumberForOffset(textArea.ActiveTextAreaControl.Caret.Offset);
             if (customLineManager.IsReadOnly(lineNumber, false) == false)
             {
-                new ICSharpCode.TextEditor.Actions.DeleteLine().Execute(textarea.ActiveTextAreaControl.TextArea);
-                textarea.Refresh();
+                //new ICSharpCode.TextEditor.Actions.DeleteToLineEnd().Execute(textArea.ActiveTextAreaControl.TextArea);
+                TextAreaControl textControl = textArea.ActiveTextAreaControl;
+                Caret caret = textControl.Caret;
+                IDocument doc = textControl.Document;
+                int line = caret.Line;
+                LineSegment lineSegment = doc.GetLineSegment(line);
+
+                textArea.BeginUpdate();
+                doc.UndoStack.StartUndoGroup();
+
+                // Remove the whole line where the caret currently is
+                doc.Remove(lineSegment.Offset, lineSegment.Length);
+
+                // move the caret to the beginning of the line
+                caret.Position = doc.OffsetToPosition(lineSegment.Offset);
+                doc.UndoStack.EndUndoGroup();
+                textArea.EndUpdate();
+
+                // Now try to remove the line itself but be careful of the read-only embed sections!
+                if (customLineManager.IsReadOnly(lineNumber + 1, false) == true && customLineManager.IsReadOnly(lineNumber - 1, false) == false)
+                {
+                    // The line below this one is read only.and the line above isn't
+                    // That means we have just deleted the last line of an embed point
+                    new ICSharpCode.TextEditor.Actions.Backspace().Execute(textArea.ActiveTextAreaControl.TextArea);
+                    new ICSharpCode.TextEditor.Actions.Home().Execute(textArea.ActiveTextAreaControl.TextArea);
+                }
+                else if (customLineManager.IsReadOnly(lineNumber + 1, false) == false)
+                {
+                    // The line below isn't read only
+                    new ICSharpCode.TextEditor.Actions.Delete().Execute(textArea.ActiveTextAreaControl.TextArea);
+                }
+
+                //doc.RequestUpdate(new TextAreaUpdate(TextAreaUpdateType.WholeTextArea));
+                //doc.CommitUpdate();
+                textArea.Refresh();
             }
         }
     }
