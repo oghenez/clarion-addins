a8df91f84bd05b8288731bb775b265b6001cf796
brahn <brahn@clarionedge.com>
2011-06-04 11:01:41 +0000

    Release 0.2, 04 June 2011 CHANGE:	Preparing for public release of source code. Making the VS project work without dependencies, adding docs, etc INFO:	This project requires the ZetaColorEditor
    Release 0.1, 16 Oct 2010
    INFO:	Public Release
    BLOG:	Insert Clarion Color addin

---
+++ InsertClarionColor/Src/ShowColorDialog.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Windows.Forms;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.DefaultEditor.Gui.Editor;
+using ICSharpCode.SharpDevelop.Gui;
+using ICSharpCode.TextEditor;
+using ZetaColorEditor.Runtime;
+
+namespace ClarionEdge.InsertClarionColor
+{
+    public class ShowColorDialog : AbstractMenuCommand
+    {
+        public override void Run()
+        {
+            IWorkbenchWindow window = WorkbenchSingleton.Workbench.ActiveWorkbenchWindow;
+
+            if (window == null || !(window.ViewContent is ITextEditorControlProvider))
+            {
+                return;
+            }
+            TextEditorControl textarea = ((ITextEditorControlProvider)window.ViewContent).TextEditorControl;
+
+            using (ColorEditorForm cd = new ColorEditorForm())
+            {
+                if (cd.ShowDialog(ICSharpCode.SharpDevelop.Gui.WorkbenchSingleton.MainForm) == DialogResult.OK)
+                {                 
+                    string ext = Path.GetExtension(textarea.FileName).ToLowerInvariant();
+                    string colorstr;
+                    colorstr = string.Format("00{0:X2}{1:X2}{2:X2}h", cd.SelectedColor.B, cd.SelectedColor.G, cd.SelectedColor.R);
+
+                    textarea.Document.Insert(textarea.ActiveTextAreaControl.Caret.Offset, colorstr);
+                    int lineNumber = textarea.Document.GetLineNumberForOffset(textarea.ActiveTextAreaControl.Caret.Offset);
+                    textarea.ActiveTextAreaControl.Caret.Column += colorstr.Length;
+                    textarea.Document.RequestUpdate(new TextAreaUpdate(TextAreaUpdateType.SingleLine, new TextLocation(0, lineNumber)));
+                    textarea.Document.CommitUpdate();
+                }
+            }
+        }
+    }
+
+}
