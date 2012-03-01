a6056b9dcb55a058a74dfc8f854b1673628490a2
brahn@clarionedge.com <brahn@clarionedge.com@b60aab8c-cd5c-1699-4943-ff1c4fd65dd7>
2012-03-01 11:39:50 +0000

    All projects that have been updated. Please see the release_notes.xml files for full details

--- trunk/EditorMacro/Src/EditorMacrosPadControl.cs
+++ trunk/EditorMacro/Src/EditorMacrosPadControl.cs
@@ -1,164 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Text;
+using System.Diagnostics;
+using System.Threading;
 using System.Windows.Forms;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop;
+using ICSharpCode.SharpDevelop.DefaultEditor.Gui.Editor;
 using ICSharpCode.SharpDevelop.Gui;
 using MouseKeyboardLibrary;
-using ICSharpCode.SharpDevelop.DefaultEditor.Gui.Editor;
-using ICSharpCode.TextEditor;
-using ICSharpCode.Core;
-using System.Threading;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-using ICSharpCode.SharpDevelop;
+using ClarionEdge.EditorMacros;
 
 namespace ClarionEdge.EditorMacros
 {
     public partial class EditorMacrosPadControl : UserControl
     {
-        BindingList<MacroEvent> events = new BindingList<MacroEvent>();
-        int lastTimeRecorded = 0;
-
-        KeyboardHook keyboardHook = new KeyboardHook();
-        TextArea textArea;
-        private Keys _lastKeyDownKeyCode = Keys.None;
+        private MacroRecording _macro;
 
         public EditorMacrosPadControl()
         {
+            LoggingService.Debug("EditorMacrosPadControl Constructor");
+
             InitializeComponent();
-            //headerGroup.ValuesPrimary.Image = CommonResources.Properties.famfamfam_silk.famfamfam_silk_script_edit;
-            buttonPlayBack.Enabled = false;
-
-            listBoxDebug.DataSource = events;
+            cbDisableCCWhileRecording.Checked = PropertyService.Get<bool>("ClarionEdge.EditorMacros.cbDisableCCWhileRecording", false);
+            _macro = new MacroRecording();
+            listBoxDebug.DataSource = _macro.Events;
             listBoxDebug.DisplayMember = "DisplayString";
-
-            keyboardHook.KeyDown += new System.Windows.Forms.KeyEventHandler(keyboardHook_KeyDown);
-            keyboardHook.KeyUp += new System.Windows.Forms.KeyEventHandler(keyboardHook_KeyUp);
-
-        }
-
-        private void buttonStart_Click(object sender, EventArgs e)
-        {
-            if (buttonStartRecording.Text == "Stop")
+            if (_macro.Events.Count == 0)
             {
-                buttonStartRecording.Image = ClarionEdge.EditorMacros.Properties.Resources.record;
-                buttonStartRecording.Text = "Start";
-                keyboardHook.Stop();
-                if (events.Count > 0)
-                {
-                    buttonPlayBack.Enabled = true;
-                }
-                if (textArea == null)
-                {
-                    return;
-                }
-            }
-            else
-            {
-                buttonStartRecording.Image = ClarionEdge.EditorMacros.Properties.Resources.stop;
-                buttonStartRecording.Text = "Stop";
-                IWorkbenchWindow window = WorkbenchSingleton.Workbench.ActiveWorkbenchWindow;
-                if (window == null) return;
-                ITextEditorControlProvider provider = window.ActiveViewContent as ITextEditorControlProvider;
-                if (provider == null)
-                {
-                    MessageBox.Show("Macros can only be recorded when there is an editor window open!");
-                    return;
-                }
-
                 buttonPlayBack.Enabled = false;
-                textArea = provider.TextEditorControl.ActiveTextAreaControl.TextArea;
-
-                events.Clear();
-                lastTimeRecorded = Environment.TickCount;
-
-                keyboardHook.Start();
-                WorkbenchSingleton.Workbench.ActiveWorkbenchWindow.SelectWindow();
             }
         }
 
-        void keyboardHook_KeyDown(object sender, KeyEventArgs e)
+        public void PlayCurrentMacro()
         {
-            if (SkipRepeatedEvents(e.KeyCode) == true && e.KeyCode == _lastKeyDownKeyCode)
-                return;
-
-            events.Add(
-                new MacroEvent(
-                    MacroEventType.KeyDown,
-                    e,
-                    Environment.TickCount - lastTimeRecorded
-                ));
-
-            lastTimeRecorded = Environment.TickCount;
-            _lastKeyDownKeyCode = e.KeyCode;
-
-        }
-
-        private bool SkipRepeatedEvents(Keys keys)
-        {
-            switch (keys)
-            {
-                case Keys.LControlKey:
-                case Keys.RControlKey:
-                case Keys.Control:
-                case Keys.ControlKey:
-                case Keys.Shift:
-                case Keys.ShiftKey:
-                case Keys.LShiftKey:
-                case Keys.RShiftKey:
-                case Keys.Alt:
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
-        void keyboardHook_KeyUp(object sender, KeyEventArgs e)
-        {
-            _lastKeyDownKeyCode = Keys.None;
-
-            events.Add(
-                new MacroEvent(
-                    MacroEventType.KeyUp,
-                    e,
-                    Environment.TickCount - lastTimeRecorded
-                ));
-
-            lastTimeRecorded = Environment.TickCount;
-
-        }
-
-        private void buttonStartPlayback_Click(object sender, EventArgs e)
-        {
-
-            bool originalCC = CodeCompletionOptions.EnableCodeCompletion;
-            CodeCompletionOptions.EnableCodeCompletion = false;
-            WorkbenchSingleton.Workbench.ActiveWorkbenchWindow.SelectWindow();
-
-            foreach (MacroEvent macroEvent in events)
-            {
-                switch (macroEvent.MacroEventType)
-                {
-                    case MacroEventType.KeyDown:
-                        {
-                            KeyEventArgs keyArgs = (KeyEventArgs)macroEvent.EventArgs;
-                            KeyboardSimulator.KeyDown(keyArgs.KeyCode);
-                        }
-                        break;
-                    case MacroEventType.KeyUp:
-                        {
-                            KeyEventArgs keyArgs = (KeyEventArgs)macroEvent.EventArgs;
-                            KeyboardSimulator.KeyUp(keyArgs.KeyCode);
-                        }
-                        break;
-                    default:
-                        break;
-                }
-            }
-            CodeCompletionOptions.EnableCodeCompletion = originalCC;
+            buttonPlayBack.PerformClick();
         }
 
         public void GainFocus()
@@ -167,6 +42,38 @@ namespace ClarionEdge.EditorMacros
             {
                 buttonPlayBack.Select();
             }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (buttonStartRecording.Text == "Stop")
+            {
+                buttonStartRecording.Image = ClarionEdge.EditorMacros.Properties.Resources.record;
+                buttonStartRecording.Text = "Start";
+                _macro.StopRecording();
+                if (_macro.Events.Count > 0)
+                    buttonPlayBack.Enabled = true;
+            }
+            else
+            {
+                IWorkbenchWindow window = WorkbenchSingleton.Workbench.ActiveWorkbenchWindow;
+                if (window == null || !(window.ViewContent is ITextEditorControlProvider))
+                {
+                    MessageBox.Show("Macros can only be recorded when there is an editor window open!");
+                    return;
+                }
+                buttonStartRecording.Image = ClarionEdge.EditorMacros.Properties.Resources.stop;
+                buttonStartRecording.Text = "Stop";
+                buttonPlayBack.Enabled = false;
+                WorkbenchSingleton.Workbench.ActiveWorkbenchWindow.SelectWindow();
+                _macro.StartRecording();
+            }
+        }
+
+        private void buttonStartPlayback_Click(object sender, EventArgs e)
+        {
+            listBoxDebug.Select();
+            _macro.Play();
         }
 
         private void listBoxDebug_MouseDown(object sender, MouseEventArgs e)
@@ -200,7 +107,7 @@ namespace ClarionEdge.EditorMacros
             {
                 if (listBoxDebug.SelectedIndex >= 0)
                 {
-                    events.RemoveAt(listBoxDebug.SelectedIndex);
+                    _macro.Events.RemoveAt(listBoxDebug.SelectedIndex);
                     listBoxDebug.ClearSelected();
                 }
             }
@@ -209,5 +116,13 @@ namespace ClarionEdge.EditorMacros
                 LoggingService.Debug("exception=" + ex.Message);
             }
         }
+
+        private void cbDisableCCWhileRecording_CheckedChanged(object sender, EventArgs e)
+        {
+            PropertyService.Set<bool>("ClarionEdge.EditorMacros.cbDisableCCWhileRecording", cbDisableCCWhileRecording.Checked);
+        }
     }
+
+
+
 }
